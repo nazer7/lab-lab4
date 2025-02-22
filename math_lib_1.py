f3effdd509245d4a2072bfa3c63f80f2efdff835
nazer7 <nazerkebekbosyn932@gmail.com>
2025-02-22 08:52:46 +0300

    Add files via upload

--- math_lib_1.py
+++ math_lib_1.py
@@ -1,8 +1,6 @@
-def area(height: float, base1: float, base2: float) -> float:
-    return 0.5 * (base1 + base2) * height
-height = float(input("Enter height: "))
-base1 = float(input("Enter base, first value: "))
-base2 = float(input("Enter base, second value: "))
-
-area = area(height, base1, base2)
-print(f"Expected Output: {area:.1f}")
+import math
+def degree_to_radian(degree: float) -> float:
+    return degree * (math.pi / 180)
+degree = float(input("Enter degree: "))
+radian = degree_to_radian(degree)
+print(f"Output radian: {radian:.6f}")
